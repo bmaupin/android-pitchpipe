a7f143ec051e8fb11b454b7337c54a3192e734f3
bmaupin <bmaupincode@gmail.com>
2015-05-31 17:24:28 -0400

    Generate a terrible beep: check

---
+++ app/src/main/java/ca/bmaupin/pitchpipe/MainActivity.java
@@ -0,0 +1,244 @@
+package ca.bmaupin.pitchpipe;
+
+import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+
+    private final int duration = 3; // seconds
+    private final int sampleRate = 8000;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+    private final double freqOfTone = 440; // 82.41; // 440; // hz
+
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+
+    Handler handler = new Handler();
+    boolean isTonePlaying = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Use a new tread as this can take a while
+        final Thread toneThread = new Thread(new Runnable() {
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+
+                    public void run() {
+                        playSound();
+                    }
+                });
+            }
+        });
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toneThread.start();
+/*
+                if (!isTonePlaying) {
+                    toneThread.start();
+                    isTonePlaying = true;
+                    Log.d(TAG, "onCreate: start playing tone");
+                } else {
+                    toneThread.stop();
+                    isTonePlaying = false;
+                    Log.d(TAG, "onCreate: stop playing tone");
+                }
+*/
+            }
+        });
+    }
+
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Use a new tread as this can take a while
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+
+                    public void run() {
+                        playSound();
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+    */
+
+    void genTone(){
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    void playSound(){
+        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
+    }
+
+    /*
+    public class PlaySound extends Activity {
+        // originally from http://marblemice.blogspot.com/2010/04/generate-and-play-tone-in-android.html
+        // and modified by Steve Pomeroy <steve@staticfree.info>
+        private final int duration = 3; // seconds
+        private final int sampleRate = 8000;
+        private final int numSamples = duration * sampleRate;
+        private final double sample[] = new double[numSamples];
+        private final double freqOfTone = 440; // hz
+
+        private final byte generatedSnd[] = new byte[2 * numSamples];
+
+        Handler handler = new Handler();
+        Thread toneThread;
+        boolean isTonePlaying = false;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // Use a new tread as this can take a while
+            toneThread = new Thread(new Runnable() {
+                public void run() {
+                    genTone();
+                    handler.post(new Runnable() {
+
+                        public void run() {
+                            playSound();
+                        }
+                    });
+                }
+            });
+
+            final Button button = (Button) findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (!isTonePlaying) {
+                        toneThread.start();
+                        isTonePlaying = true;
+                        Log.d(TAG, "onCreate: start playing tone");
+                    } else {
+                        toneThread.stop();
+                        isTonePlaying = false;
+                        Log.d(TAG, "onCreate: stop playing tone");
+                    }
+                }
+            });
+        }
+
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+
+            // Use a new tread as this can take a while
+            final Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    genTone();
+                    handler.post(new Runnable() {
+
+                        public void run() {
+                            playSound();
+                        }
+                    });
+                }
+            });
+            thread.start();
+        }
+
+
+        void genTone(){
+            // fill out the array
+            for (int i = 0; i < numSamples; ++i) {
+                sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+            }
+
+            // convert to 16 bit pcm sound array
+            // assumes the sample buffer is normalised.
+            int idx = 0;
+            for (final double dVal : sample) {
+                // scale to maximum amplitude
+                final short val = (short) ((dVal * 32767));
+                // in 16 bit wav PCM, first byte is the low order byte
+                generatedSnd[idx++] = (byte) (val & 0x00ff);
+                generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+            }
+        }
+
+        void playSound(){
+            final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                    sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                    AudioTrack.MODE_STATIC);
+            audioTrack.write(generatedSnd, 0, generatedSnd.length);
+            audioTrack.play();
+        }
+    }
+    */
+
+}
