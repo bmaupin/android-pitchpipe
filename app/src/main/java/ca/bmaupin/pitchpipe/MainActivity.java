735871d036af135db3207118dddb648c04d53607
bmaupin <bmaupincode@gmail.com>
2016-03-15 16:27:15 -0400

    Get basic MIDI working with library

--- app/src/main/java/ca/bmaupin/pitchpipe/MainActivity.java
+++ app/src/main/java/ca/bmaupin/pitchpipe/MainActivity.java
@@ -4,241 +4,242 @@ import android.app.Activity;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.billthefarmer.mididriver.MidiDriver;
 
 
-public class MainActivity extends Activity {
-    private static final String TAG = "MainActivity";
+public class MainActivity extends Activity    implements View.OnTouchListener, View.OnClickListener,
+        MidiDriver.OnMidiStartListener
+{
+    private TextView text;
 
-    private final int duration = 3; // seconds
-    private final int sampleRate = 8000;
-    private final int numSamples = duration * sampleRate;
-    private final double sample[] = new double[numSamples];
-    private final double freqOfTone = 440; // 82.41; // 440; // hz
-
-    private final byte generatedSnd[] = new byte[2 * numSamples];
-
-    Handler handler = new Handler();
-    boolean isTonePlaying = false;
+    protected MidiDriver midi;
+    protected MediaPlayer player;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create midi driver
+
+        midi = new MidiDriver();
+
+        // Set on touch listener
+
+        View v = findViewById(R.id.button1);
+        if (v != null)
+            v.setOnTouchListener(this);
+
+        v = findViewById(R.id.button2);
+        if (v != null)
+            v.setOnTouchListener(this);
+
+/*
+        v = findViewById(R.id.button3);
+        if (v != null)
+            v.setOnClickListener(this);
+
+        v = findViewById(R.id.button4);
+        if (v != null)
+            v.setOnClickListener(this);
+*/
+
+//        text = (TextView)findViewById(R.id.textView2);
+
+        // Set on midi start listener
+
+        if (midi != null)
+            midi.setOnMidiStartListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    // On resume
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
 
-        // Use a new tread as this can take a while
-        final Thread toneThread = new Thread(new Runnable() {
-            public void run() {
-                genTone();
-                handler.post(new Runnable() {
+        // Start midi
 
-                    public void run() {
-                        playSound();
-                    }
-                });
-            }
-        });
+        if (midi != null)
+            midi.start();
+    }
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                toneThread.start();
+    // On pause
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        // Stop midi
+
+        if (midi != null)
+            midi.stop();
+
+        // Stop player
+
+        if (player != null)
+            player.stop();
+    }
+
+    // On touch
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
+    {
+        int action = event.getAction();
+        int id = v.getId();
+
+        switch (action)
+        {
+            // Down
+
+            case MotionEvent.ACTION_DOWN:
+                switch (id)
+                {
+                    case R.id.button1:
+                        sendMidi(0x90, 48, 63);
+                        sendMidi(0x90, 52, 63);
+                        sendMidi(0x90, 55, 63);
+                        break;
+
+                    case R.id.button2:
+                        sendMidi(0x90, 55, 63);
+                        sendMidi(0x90, 59, 63);
+                        sendMidi(0x90, 62, 63);
+                        break;
+
+                    default:
+                        return false;
+                }
+                break;
+
+            // Up
+
+            case MotionEvent.ACTION_UP:
+                switch (id)
+                {
+                    case R.id.button1:
+                        sendMidi(0x80, 48, 0);
+                        sendMidi(0x80, 52, 0);
+                        sendMidi(0x80, 55, 0);
+                        break;
+
+                    case R.id.button2:
+                        sendMidi(0x80, 55, 0);
+                        sendMidi(0x80, 59, 0);
+                        sendMidi(0x80, 62, 0);
+                        break;
+
+                    default:
+                        return false;
+                }
+                break;
+
+            default:
+                return false;
+        }
+
+        return false;
+    }
+
+    // On click
+
+    @Override
+    public void onClick(View v)
+    {
+        int id = v.getId();
+
+        switch (id)
+        {
 /*
-                if (!isTonePlaying) {
-                    toneThread.start();
-                    isTonePlaying = true;
-                    Log.d(TAG, "onCreate: start playing tone");
-                } else {
-                    toneThread.stop();
-                    isTonePlaying = false;
-                    Log.d(TAG, "onCreate: stop playing tone");
+            case R.id.button3:
+                if (player != null)
+                {
+                    player.stop();
+                    player.release();
                 }
+
+                player = MediaPlayer.create(this, R.raw.ants);
+                player.start();
+                break;
+
+            case R.id.button4:
+                if (player != null)
+                    player.stop();
+                break;
 */
-            }
-        });
+        }
     }
 
-    /*
+    // Listener for sending initial midi messages when the Sonivox
+    // synthesizer has been started, such as program change.
+
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onMidiStart()
+    {
+        // Program change - harpsicord
 
-        // Use a new tread as this can take a while
-        final Thread thread = new Thread(new Runnable() {
-            public void run() {
-                genTone();
-                handler.post(new Runnable() {
+        sendMidi(0xc0, 6);
 
-                    public void run() {
-                        playSound();
-                    }
-                });
-            }
-        });
-        thread.start();
-    }
-    */
+        // Get the config
+/*
+        int config[] = midi.config();
+        String format =
+                "maxVoices = %d\nnumChannels = %d\n" +
+                        "sampleRate = %d\nmixBufferSize = %d";
 
-    void genTone(){
-        // fill out the array
-        for (int i = 0; i < numSamples; ++i) {
-            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
-        }
+        String info = String.format(format, config[0], config[1],
+                config[2], config[3]);
 
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        int idx = 0;
-        for (final double dVal : sample) {
-            // scale to maximum amplitude
-            final short val = (short) ((dVal * 32767));
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedSnd[idx++] = (byte) (val & 0x00ff);
-            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-
-        }
+        if (text != null)
+            text.setText(info);
+*/
     }
 
-    void playSound(){
-        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
-                AudioTrack.MODE_STATIC);
-        audioTrack.write(generatedSnd, 0, generatedSnd.length);
-        audioTrack.play();
+    // Send a midi message
+
+    protected void sendMidi(int m, int p)
+    {
+        byte msg[] = new byte[2];
+
+        msg[0] = (byte) m;
+        msg[1] = (byte) p;
+
+        midi.write(msg);
     }
 
-    /*
-    public class PlaySound extends Activity {
-        // originally from http://marblemice.blogspot.com/2010/04/generate-and-play-tone-in-android.html
-        // and modified by Steve Pomeroy <steve@staticfree.info>
-        private final int duration = 3; // seconds
-        private final int sampleRate = 8000;
-        private final int numSamples = duration * sampleRate;
-        private final double sample[] = new double[numSamples];
-        private final double freqOfTone = 440; // hz
+    // Send a midi message
 
-        private final byte generatedSnd[] = new byte[2 * numSamples];
+    protected void sendMidi(int m, int n, int v)
+    {
+        byte msg[] = new byte[3];
 
-        Handler handler = new Handler();
-        Thread toneThread;
-        boolean isTonePlaying = false;
+        msg[0] = (byte) m;
+        msg[1] = (byte) n;
+        msg[2] = (byte) v;
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            // Use a new tread as this can take a while
-            toneThread = new Thread(new Runnable() {
-                public void run() {
-                    genTone();
-                    handler.post(new Runnable() {
-
-                        public void run() {
-                            playSound();
-                        }
-                    });
-                }
-            });
-
-            final Button button = (Button) findViewById(R.id.button);
-            button.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    if (!isTonePlaying) {
-                        toneThread.start();
-                        isTonePlaying = true;
-                        Log.d(TAG, "onCreate: start playing tone");
-                    } else {
-                        toneThread.stop();
-                        isTonePlaying = false;
-                        Log.d(TAG, "onCreate: stop playing tone");
-                    }
-                }
-            });
-        }
-
-
-        @Override
-        protected void onResume() {
-            super.onResume();
-
-            // Use a new tread as this can take a while
-            final Thread thread = new Thread(new Runnable() {
-                public void run() {
-                    genTone();
-                    handler.post(new Runnable() {
-
-                        public void run() {
-                            playSound();
-                        }
-                    });
-                }
-            });
-            thread.start();
-        }
-
-
-        void genTone(){
-            // fill out the array
-            for (int i = 0; i < numSamples; ++i) {
-                sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
-            }
-
-            // convert to 16 bit pcm sound array
-            // assumes the sample buffer is normalised.
-            int idx = 0;
-            for (final double dVal : sample) {
-                // scale to maximum amplitude
-                final short val = (short) ((dVal * 32767));
-                // in 16 bit wav PCM, first byte is the low order byte
-                generatedSnd[idx++] = (byte) (val & 0x00ff);
-                generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-
-            }
-        }
-
-        void playSound(){
-            final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                    sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
-                    AudioTrack.MODE_STATIC);
-            audioTrack.write(generatedSnd, 0, generatedSnd.length);
-            audioTrack.play();
-        }
+        midi.write(msg);
     }
-    */
-
 }
