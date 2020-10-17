ad8b2d70f33f57ae796e0b37c7ad71e9e1c04938
bmaupin <bmaupin@users.noreply.github.com>
2020-10-17 15:04:39 -0400

    Wire up button to play MIDI note

--- app/src/main/java/ca/bmaupin/pitchpipe/MainActivity.kt
+++ app/src/main/java/ca/bmaupin/pitchpipe/MainActivity.kt
@@ -1,11 +1,77 @@
 package ca.bmaupin.pitchpipe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+import org.billthefarmer.mididriver.GeneralMidiConstants
+import org.billthefarmer.mididriver.MidiConstants
+import org.billthefarmer.mididriver.MidiDriver
+
+// TODO: add a setting to change the duration
+private const val NOTE_DURATION: Long = 5000
+private const val NOTE_VELOCITY = 95
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var midi: MidiDriver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        midi = MidiDriver()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        midi.stop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        midi.start()
+        // This needs to be called every time the app is resumed, otherwise it will reset to the default instrument
+        setMidiInstrument()
+    }
+
+    private fun setMidiInstrument() {
+        sendMidi(MidiConstants.PROGRAM_CHANGE, GeneralMidiConstants.BAG_PIPE)
+    }
+
+    fun onPlayButtonClick(view: View) {
+        playNote(64);
+    }
+
+    private fun playNote(notePitch: Int) {
+        sendMidi(MidiConstants.NOTE_ON, notePitch, NOTE_VELOCITY);
+
+        // Execute code after a certain length of time
+        // http://stackoverflow.com/a/3039718/399105
+        val handler = Handler()
+        handler.postDelayed(Runnable { stopNote(notePitch) }, NOTE_DURATION)
+    }
+
+    private fun stopNote(notePitch: Int) {
+        sendMidi(MidiConstants.NOTE_OFF, notePitch, NOTE_VELOCITY)
+    }
+
+    // Send a midi message, 2 bytes
+    private fun sendMidi(m: Byte, n: Byte) {
+        val msg = ByteArray(2)
+        msg[0] = m
+        msg[1] = n
+        midi.queueEvent(msg)
+    }
+
+    // Send a midi message, 3 bytes
+    private fun sendMidi(m: Byte, n: Int, v: Int) {
+        val msg = ByteArray(3)
+        msg[0] = m
+        msg[1] = n.toByte()
+        msg[2] = v.toByte()
+        midi.queueEvent(msg)
     }
 }
