4b07f75d5c994b484f863c3b428bfa8f46503b4c
bmaupin <bmaupin@users.noreply.github.com>
2022-02-23 13:14:03 -0500

    feat: First step at migrating to sherlockmidi
    The notes are successfully played! Still need to implement note duration, pausing/resuming activity, include a proper sound font...

--- app/src/main/java/io/bmaupin/pitchpipe/MainActivity.kt
+++ app/src/main/java/io/bmaupin/pitchpipe/MainActivity.kt
@@ -7,40 +7,73 @@ import android.view.View
 import android.widget.ViewFlipper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import cn.sherlock.com.sun.media.sound.SF2Soundbank
+import cn.sherlock.com.sun.media.sound.SoftSynthesizer
 import com.google.android.material.snackbar.Snackbar
+import jp.kshoji.javax.sound.midi.MidiUnavailableException
+import jp.kshoji.javax.sound.midi.Receiver
+import jp.kshoji.javax.sound.midi.ShortMessage
 import org.billthefarmer.mididriver.GeneralMidiConstants
 import org.billthefarmer.mididriver.MidiConstants
 import org.billthefarmer.mididriver.MidiDriver
+import java.io.IOException
 import java.util.*
-import kotlin.concurrent.schedule
 
 private const val NOTE_DURATION: Long = 5000
 private const val NOTE_VELOCITY = 95
 
 class MainActivity : AppCompatActivity() {
+//    TODO
     private lateinit var midi: MidiDriver
     private var stopNoteTimer = Timer()
+
+    private lateinit var synth: SoftSynthesizer
+    private lateinit var recv: Receiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        midi = MidiDriver()
+//        midi = MidiDriver()
+
+//        TODO: remove error handling; let it crash early
+        try {
+            val soundFont: SF2Soundbank =
+                SF2Soundbank(
+                    assets.open("SmallTimGM6mb.sf2")
+                )
+            synth = SoftSynthesizer()
+            synth.open()
+            synth.loadAllInstruments(soundFont)
+            synth.channels[0].programChange(0)
+            recv = synth.receiver
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: MidiUnavailableException) {
+            e.printStackTrace()
+        }
     }
 
     override fun onPause() {
         super.onPause()
 
-        midi.stop()
+//        TODO
+//        midi.stop()
     }
 
     override fun onResume() {
         super.onResume()
 
-        midi.start()
+//        TODO
+//        midi.start()
         // This needs to be called every time the app is resumed, otherwise it will reset to the default instrument
-        setMidiInstrument()
+//        setMidiInstrument()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+            synth.close()
     }
 
     private fun setMidiInstrument() {
@@ -67,9 +100,10 @@ class MainActivity : AppCompatActivity() {
         Snackbar.make(view, view.tag.toString(), Snackbar.LENGTH_SHORT)
             .show()
 
-        // Stop all previous notes
-        stopNoteTimer.cancel()
-        stopAllMidiNotes()
+//        TODO
+//        // Stop all previous notes
+//        stopNoteTimer.cancel()
+//        stopAllMidiNotes()
 
         val noteButtons = findViewById<ViewFlipper>(R.id.note_buttons)
         var index = 0
@@ -98,24 +132,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun playPitchPipeNote(view: View) {
-        // Stop all previous notes
-        stopNoteTimer.cancel()
-        stopAllMidiNotes()
+//        // Stop all previous notes
+//        stopNoteTimer.cancel()
+//        stopAllMidiNotes()
 
         // Play the new note
         val notePitch = view.tag.toString().toInt()
         playMidiNote(notePitch);
 
-        // Schedule the current note to stop; midi is a streaming protocol and so the duration cannot be set when the note is played
-        // https://stackoverflow.com/a/54352394/399105
-        stopNoteTimer = Timer()
-        stopNoteTimer.schedule(NOTE_DURATION) {
-            stopAllMidiNotes()
-        }
+//        // Schedule the current note to stop; midi is a streaming protocol and so the duration cannot be set when the note is played
+//        // https://stackoverflow.com/a/54352394/399105
+//        stopNoteTimer = Timer()
+//        stopNoteTimer.schedule(NOTE_DURATION) {
+//            stopAllMidiNotes()
+//        }
     }
 
     private fun playMidiNote(notePitch: Int) {
-        sendMidi(MidiConstants.NOTE_ON, notePitch, NOTE_VELOCITY);
+        val msg: ShortMessage = ShortMessage()
+        msg.setMessage(ShortMessage.NOTE_ON, 0, notePitch, NOTE_VELOCITY)
+        recv.send(msg, -1)
+
+//        sendMidi(MidiConstants.NOTE_ON, notePitch, NOTE_VELOCITY);
     }
 
     private fun stopAllMidiNotes() {
